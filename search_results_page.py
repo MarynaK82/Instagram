a245cd3917140e2cf6d04f7270bc677ca7baad12
MarynaK82 <56485160+MarynaK82@users.noreply.github.com>
2019-10-21 20:09:40 +0300

    Add files via upload

---
+++ search_results_page.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.common.by import By
+from pages.base_page import BasePage
+
+class MainPage(BasePage):
+    BUTTON_NOT_NOW = (By.XPATH, "//button[text() = 'Not Now']")
+    FIELD_SEARCH = (By.XPATH, "//input[@placeholder = 'Search']")
+
+    def type_in_search_field(self, param):
+        self.type_in(self.FIELD_SEARCH, "#fitness")
+
+    def click_result_with_text(self, text):
+        RESULT = (By.XPATH, "")
